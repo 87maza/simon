666e55f59a7024b6641a2d5197f6fdd293808c43
87maza <87maza@gmail.com>
2016-04-22 00:21:11 -0700

    win logic for round 1 complete, need to create recursive function incrementer

--- js/script.js
+++ js/script.js
@@ -11,22 +11,40 @@ var winningCombos = [];
 var humanCombos = [];
 var randomCalls = 0;
 var red = document.getElementById('red');
-red.addEventListener('click', function(){redSound.play(); humanCombos.push('red')});
+red.addEventListener('click', function(){
+	redSound.play();
+	humanCombos.push('red');
+	checkWinner();
+});
 var blue = document.getElementById('blue');
-blue.addEventListener('click', function(){blueSound.play(); humanCombos.push('blue')});
+blue.addEventListener('click', function(){
+	blueSound.play();
+ 	humanCombos.push('blue');
+	checkWinner();
+});
 var green = document.getElementById('green');
-green.addEventListener('click', function(){greenSound.play(); humanCombos.push('green')});
+green.addEventListener('click', function(){
+	greenSound.play();
+ 	humanCombos.push('green');
+ 	checkWinner();
+});
 var yellow = document.getElementById('yellow');
-yellow.addEventListener('click', function(){yellowSound.play(); humanCombos.push('yellow');});
-
+yellow.addEventListener('click', function(){
+	yellowSound.play();
+ 	humanCombos.push('yellow');
+ 	checkWinner();
+});
 
 function random() {
 	var colors = [red, blue, green, yellow];
+	var sounds = [redSound, blueSound, greenSound, yellowSound];
 	var num = Math.floor(Math.random() * 3) + 1;
 	var rand = colors[num];
+	var randSound = sounds[num];
+	console.log(randSound);
 	rand.classList.add('light');
 	winningCombos.push(rand.children[1].innerText.toLowerCase());
-	rand.click();
+	randSound.play();
 	setTimeout(function(){
 		rand.classList.remove('light');
 	}, 1000);
@@ -34,7 +52,12 @@ function random() {
 	humanCombos.pop();  //doubles down on the humanCombos, need to pop it
 }
 
-function humanTurn() {
-	alert('please click human!');
-	
+function checkWinner() {
+	if(humanCombos.length > 0) {
+		if(humanCombos.join('')===winningCombos.join('')){
+			alert('hell yeah');
+		}
+		return;
+	}
+	return;
 }
